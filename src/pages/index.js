faa34aba3c4b279926afb83663978b20ad6d26bf
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-04-20 11:42:45 +0200

    test change

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "gatsby-link";
 import './index.scss';
 
 export default () => <div style={{ color: `tomato` }}>
-    <h1>Hello Gatsby!</h1>
+    <h1>Welcome to my site!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />

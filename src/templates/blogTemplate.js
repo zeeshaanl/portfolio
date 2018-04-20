c4156269f47af67cc18ae4e413189aa2774bcb25
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-04-20 13:46:02 +0200

    add gatsby-node

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Template({
-    data, // this prop will be injected by the GraphQL query below.
+    data,
 }) {
-    const { markdownRemark } = data; // data.markdownRemark holds our post data
+    const { markdownRemark } = data;
     const { frontmatter, html } = markdownRemark;
     return (
         <div className="blog-post-container">

18ee253e88790c0b9b04054e0c144efc4e4c87c5
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-05-26 01:13:01 +0200

    add react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from "react"
 import Link from "gatsby-link";
 import { Parallax, Background } from 'react-parallax';
+import { Helmet } from "react-helmet";
 import mainLogo from '../../static/img/background.jpeg';
 import portfolioBackground from '../../static/img/portfolio-background.jpeg';
 import './index.scss';
 
 export default () => <div>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>Welcome to Zeeshaan's Portfolio</title>
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Sunflower" />
+    </Helmet>
     <Parallax
         bgImage={portfolioBackground}
         bgImageAlt=""
@@ -15,13 +22,8 @@ export default () => <div>
             Hi, I'm Zeeshaan, a fullstack developer based in Hamburg, Germany.
         </div>
     </Parallax>
-    <Parallax
-        bgImage={mainLogo}
-        bgImageAlt=""
-        strength={200}
-    >
-        <div style={{ height: '100vh' }}>
-            <Link to="/page-2/">Link</Link>
-        </div>
-    </Parallax>
+    <div style={{ height: '100vh', textAlign: 'center' }}>
+        <h1>Projects</h1>
+        <Link to="/page-2/">Link</Link>
+    </div>
 </div>

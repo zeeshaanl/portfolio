0092897c923c83c75d28bba16676df4dc05ef247
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-05-27 13:49:54 +0200

    Add new images and sections with text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Link from "gatsby-link";
 import { Parallax, Background } from 'react-parallax';
 import { Helmet } from "react-helmet";
 import mainLogo from '../../static/img/background.jpeg';
-import portfolioBackground from '../../static/img/portfolio-background.jpeg';
+import introPic from '../../static/img/intro-pic.jpg';
+import aboutPic from '../../static/img/portfolio-background.jpeg';
 import './index.scss';
 
 export default () => <div>
@@ -11,10 +12,11 @@ export default () => <div>
         <meta charSet="utf-8" />
         <title>Zeeshaan's Portfolio</title>
         <link rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Sunflower" />
+              href="https://fonts.googleapis.com/css?family=Handlee" />
     </Helmet>
     <Parallax
-        bgImage={portfolioBackground}
+        blur={{ min: -15, max: 15 }}
+        bgImage={introPic}
         bgImageAlt=""
         strength={200}
     >
@@ -22,12 +24,19 @@ export default () => <div>
             I'm Zeeshaan, a fullstack javascript developer based in Hamburg, Germany.
         </div>
     </Parallax>
-    <div style={{ height: '100vh', textAlign: 'center' }}>
-        <h1>About</h1>
-        <p>
-            I enjoy building MVPs from top to bottom and testing the potential and viability of ideas, with code that not only a mother could love.
-        </p>
-    </div>
+    <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={aboutPic}
+        bgImageAlt=""
+        strength={200}
+    >
+        <div style={{ height: '100vh', textAlign: 'center' }}>
+            <h1 className='c-header'>About</h1>
+            <h2 className='c-about-message'>
+                I enjoy building MVPs from top to bottom and testing the potential and viability of ideas, with code that not only a mother could love.
+            </h2>
+        </div>
+    </Parallax>
     <div style={{ height: '100vh', textAlign: 'center' }}>
         <h1>Projects</h1>
         <Link to="/page-2/">Link</Link>
